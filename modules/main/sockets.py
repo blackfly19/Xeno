5d2fdc4fbe76fad12d76ca410171d50eb738d603
Ankit Chawla <ankitc226@gmail.com>
2020-11-16 15:33:57 +0530

    changes in pika

--- modules/main/sockets.py
+++ modules/main/sockets.py
@@ -19,7 +19,7 @@ def disconnect():
 
 @socketio.on('mapHashID')
 def mapHashID(Hash):
-    pika_client = pika.BlockingConnection(pika.ConnectionParameters(host=MQ_URL,heartbeat=5))
+    pika_client = pika.BlockingConnection(pika.URLParameters(host=MQ_URL))
     channel = pika_client.channel()
 
     if redis_client.exists(Hash):
