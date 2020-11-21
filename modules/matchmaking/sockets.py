c5dd56441e3d301e0efa440700011b7495abc910
Ankit Chawla <ankitc226@gmail.com>
2020-11-21 14:04:48 +0530

    matchmaking

--- modules/matchmaking/sockets.py
+++ modules/matchmaking/sockets.py
@@ -2,9 +2,12 @@ from modules import socketio,redis_client
 from modules.models import User
 from flask_socketio import emit
 import json
+import time
 
 @socketio.on('matchQueue')
 def match(Hash):
+
+    time.sleep(3)
     redis_client.rpush('matchqueue',Hash)
     redis_client.incr('match_queue_count')
     while int(redis_client.get('match_queue_count').decode('utf-8')) > 1:
