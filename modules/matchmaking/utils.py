277625a985f55a5de573be963516e0ce0fc72226
Ankit Chawla <ankitc226@gmail.com>
2020-12-14 22:01:58 +0530

    matchmaking

--- modules/matchmaking/utils.py
+++ modules/matchmaking/utils.py
@@ -4,7 +4,9 @@ from flask_socketio import emit
 @async_task.task
 def Wait():
     while redis_client.ttl('matchqueue') != -1 and redis_client.ttl('matchqueue') != -2:
+        print("In while")
         continue
+    print("out of while")
 
     if redis_client.ttl('matchqueue') == -2:
         emit('matchCancel',1)
