9d702ebd229a5916336a17157c3fae8f846f50a7
Ankit Chawla <ankitc226@gmail.com>
2020-12-15 16:12:05 +0530

    New socket instance in celery

--- modules/matchmaking/utils.py
+++ modules/matchmaking/utils.py
@@ -1,6 +1,7 @@
 from modules import redis_client,REDIS_URL
 from flask_socketio import SocketIO
 from celery import Celery 
+from flask import request
 
 async_task = Celery('tasks',broker=REDIS_URL)
 socket = SocketIO(message_queue=REDIS_URL)
@@ -13,4 +14,4 @@ def Wait():
 
     if redis_client.ttl('matchqueue') == -2:
         redis_client.decr('match_queue_count')
-        socket.emit('matchCancel',1)
+        socket.emit('matchCancel',1,room=request.sid)
