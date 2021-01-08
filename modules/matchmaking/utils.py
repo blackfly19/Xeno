76fa24466afffc42cc012b70ebc3cbd6db5b2ac2
Ankit Chawla <ankitc226@gmail.com>
2021-01-08 07:11:02 +0530

    Celery test and testing emitting from celery worker 4

--- modules/matchmaking/utils.py
+++ modules/matchmaking/utils.py
@@ -15,7 +15,7 @@ def Wait(Hash):
 
     if redis_client.ttl('matchqueue') == -2:
         redis_client.decr('match_queue_count')
-        socket.emit('matchCancel',Hash)
+        socket.emit('matchCancel',Hash,room=redis_client.get(Hash))
 
 @async_task.task()
 def tryCheck():
