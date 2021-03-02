0a1e5160dfeb5bc9bbf8494138714751413de279
Ankit Chawla <ankitc226@gmail.com>
2021-03-03 02:28:01 +0530

    Change verified fromm json read data to False

--- tests/main.py
+++ tests/main.py
@@ -1,11 +1,22 @@
 from modules import create_app,socketio
 from modules.config import DevelopmentConfig
+import unittest
 
-def socketio_test():
+class XenoTest(unittest.TestCase):
+
+    def __init__(self):
+        self.app = create_app(DevelopmentConfig())
+
+    def test_main():
+        client = socketio.test_client(self.app)
+        assert client.is_connected()
+
+"""def socketio_test():
 
     app = create_app(DevelopmentConfig()) 
     flask_test_client = app.test_client()
     socketio_test_client = socketio.test_client(app,flask_test_client=flask_test_client)
-    assert socketio_test_client.is_connected()
+    assert socketio_test_client.is_connected()"""
 
-socketio_test()
+if __name__ == '__main__':
+    unittest.main()
