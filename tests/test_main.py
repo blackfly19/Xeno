c43ec56252530e633cc120e85087bce4fc3e0145
Ankit Chawla <ankitc226@gmail.com>
2021-03-03 03:12:28 +0530

    Added image verification

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,19 +4,12 @@ import unittest
 
 class XenoTest(unittest.TestCase):
 
-    def __init__(self):
+    def setUp(self):
         self.app = create_app(DevelopmentConfig())
 
-    def test_main():
+    def test_main(self):
         client = socketio.test_client(self.app)
         assert client.is_connected()
-
-"""def socketio_test():
-
-    app = create_app(DevelopmentConfig()) 
-    flask_test_client = app.test_client()
-    socketio_test_client = socketio.test_client(app,flask_test_client=flask_test_client)
-    assert socketio_test_client.is_connected()"""
 
 if __name__ == '__main__':
     unittest.main()
