fba98664a1a41694032d6394a162f4f768035ced
Ankit Chawla <ankitc226@gmail.com>
2021-03-09 03:08:02 +0530

    fixing receipts for unsent messages

--- modules/chat/sockets.py
+++ modules/chat/sockets.py
@@ -1,20 +1,25 @@
 from modules import socketio, redis_client
 from modules.global_utils import messageHandler
 from flask_socketio import emit
+from flask import request
 import json
 
 
 @socketio.on('message')
-def handleMessage(message):
-    messageHandler(message_json=message)
+def handleMessage(message_json):
+    message = json.loads(message_json)
+    messageHandler(message_json=message_json, message=message)
+    emit('receipt', message['id'], room=request.sid)
 
 
 @socketio.on('unsent')
 def unsent(messages):
     messages = json.loads(messages)
+    sender = request.sid
     for message in messages:
         message_json = json.dumps(message)
         messageHandler(message_json=message_json, message=message)
+        emit('receipt', message['id'], room=sender)
 
 
 @socketio.on('friendTyping')
--- modules/global_utils.py
+++ modules/global_utils.py
@@ -30,7 +30,6 @@ def messageHandler(message_json, message=None):
         msg = json.loads(message_json)
 
     receiver = redis_client.get(msg['friendHashID'])
-    sender = redis_client.get(msg['userHashID']).decode('utf-8')
 
     if msg['type'] != 'message':
         check_for_block = None
@@ -52,8 +51,6 @@ def messageHandler(message_json, message=None):
             receiver = receiver.decode('utf-8')
             emit(msg['type'], message_json, room=receiver)
 
-        if msg['type'] == 'message':
-            emit('receipt', msg['id'], room=sender)
     return receiver
 
 
