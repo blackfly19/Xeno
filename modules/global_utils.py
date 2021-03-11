0f0ca671814c364b40e6064fdefc0015cbe02930
Ankit Chawla <ankitc226@gmail.com>
2021-03-11 23:30:01 +0530

    Changed threshold for face recognition

--- modules/global_utils.py
+++ modules/global_utils.py
@@ -77,7 +77,10 @@ def face_verify(image_url, encoded_img):
     verify_result = face_client.face.verify_face_to_face(
         display_pic[0].face_id, camera_pic[0].face_id)
     print(verify_result.is_identical, verify_result.confidence)
-    return verify_result.is_identical
+    if verify_result.confidence > 0.42:
+        return True
+    else:
+        return False
 
 
 def convert_base64_to_url(encoded_img, imageFileName):
