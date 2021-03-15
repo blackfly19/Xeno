7747c6859fd8df26f0b9e8ccf0358a5b150dcb1b
Ankit Chawla <ankitc226@gmail.com>
2021-03-16 00:00:50 +0530

    Adding messages

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,24 +6,31 @@ import uuid
 
 class MainTest(unittest.TestCase):
 
-    def setUp(self):
-        self.app = create_app(DevelopmentConfig())
+    app = create_app(DevelopmentConfig())
 
     def test_connect(self):
         client = socketio.test_client(self.app)
         assert not client.is_connected()
-        client = socketio.test_client(self.app, query_string='?api_key=ankit')
+        client = socketio.test_client(self.app, query_string='?api_key=random')
         assert client.is_connected()
         print(client.get_received())
         client.disconnect()
 
     def test_mapHashID(self):
-        client = socketio.test_client(self.app, query_string='?api_key=ankit')
+        client = socketio.test_client(self.app, query_string='?api_key=random')
+        assert client.is_connected()
+        print(client.get_received())
         hashID = uuid.uuid1()
         client.emit('mapHashID', hashID.hex)
+        client.disconnect()
 
     def test_onlineUsers(self):
-
+        client = socketio.test_client(self.app, query_string='?api_key=random')
+        assert client.is_connected()
+        print(client.get_received())
+        client.emit('onlineUsers')
+        print(client.get_received())
+        client.disconnect()
 
 
 if __name__ == '__main__':
